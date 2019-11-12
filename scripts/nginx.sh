e26401441c5ce4bd900f01bb5077b97e443984b3
Glen Robson <glen.robson@gmail.com>
2019-11-12 23:39:52 +0000

    Adding default param

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    dir="`pwd`/storage/gdmrdigital/iiif-manifests-gdmr"
+else
+    dir=$1
+fi
 echo "Running nginx on port 80: http://localhost"
-docker build --rm -t nginx-s3:latest . && docker run --rm --name nginx-s3 -p 80:80 -v $1:/usr/share/nginx/html:ro nginx-s3:latest
+docker build -t nginx-s3:latest . && docker run --rm --name nginx-s3 -p 80:80 -v $dir:/usr/share/nginx/html:ro nginx-s3:latest
