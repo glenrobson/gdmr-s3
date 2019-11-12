8995ab31dcd16b5f29a5990ac27725554f0d85a6
Glen Robson <glen.robson@gmail.com>
2019-11-12 23:39:27 +0000

    Adding cloudfront config

--- scripts/sync.py
+++ scripts/sync.py
@@ -8,12 +8,18 @@ import configargparse
 import datetime
 import json
 import magic
+import re
+import waitForCloudfront
 from pytz import timezone
 from dateutil.tz import tzlocal
 from tzlocal import get_localzone
 
 localtimezone = get_localzone()
 mime = magic.Magic(mime=True)
+ignoreList = ['.swp', 'iiif-photowall','.DS_Store']
+
+CACHE_HEADER='max-age=10800' # 3 hours
+NO_CACHE_HEADER='no-cache'
 
 def isIgnore(name):
     if name == '.DS_Store':
@@ -45,21 +51,33 @@ def listfiles(dir, prefix):
 
                 files[filedata['id']] = filedata
     return files
-def putfile(s3, bucket, localfile, remote):
-    s3.put_object(Bucket=bucket,ACL='public-read',Body=open(localfile['path'], 'rb'),Key=remote, ContentType=localfile['mime-type'], ContentDisposition='inline')
+def putfile(s3, bucket, localfile, remote, noCacheFileList, changes):
+    cache = ''
+    if remote in noCacheFileList:
+        cache = NO_CACHE_HEADER
+    else:
+        # flag to update cache
+        changes.append("/" + key)
+        cache = CACHE_HEADER
+    s3.put_object(Bucket=bucket,ACL='public-read',Body=open(localfile['path'], 'rb'),Key=remote, CacheControl=cache, ContentType=localfile['mime-type'], ContentDisposition='inline')
 
 def deletefile(s3, bucket, key):
     s3.delete_object(Bucket=bucket,Key=key)
 
 if __name__ == "__main__":
-    parser = configargparse.ArgParser(description='Create a manifest from a directory of images.', default_config_files=['config/sync.config'])
+    parser = configargparse.ArgParser(description='Sync local files with s3.', default_config_files=['config/sync.config'])
     parser.add('--root-dir',nargs=1,  help='Location to sync')
     parser.add('--check-mime',nargs=1, help='Check mime matches local')
+    parser.add('--wait-for-cache',nargs=1, help='Check mime matches local')
+    parser.add('--in-development',nargs='*', help='Supply files that should not be cached')
     # test program with out making changes
     simulate=False
     options = parser.parse_args()
     print(options)
     root = options.root_dir[0]
+    waitForCache = options.wait_for_cache[0] == "true"
+    noCacheFiles = options.in_development
+    print(noCacheFiles)
     if options.check_mime:
         checkmime = True
     else:
@@ -73,7 +91,11 @@ if __name__ == "__main__":
         config.read(os.path.join(account,'.aws-credentials.ini'))
         section='credentials'
         s3client = boto3.client('s3',aws_access_key_id=config.get(section,'aws_access_key_id'), aws_secret_access_key=config.get(section,'aws_secret_access_key'), region_name=config.get(section,'region_name'))
+        cloudfront = boto3.client('cloudfront',aws_access_key_id=config.get(section,'aws_access_key_id'), aws_secret_access_key=config.get(section,'aws_secret_access_key'))
         localcontents = {}
+        # Cloudfront details
+        changes = []
+        distributionId = config.get('cloud_front', 'distribution_id')
         for bucket in os.listdir(account):
             if os.path.isdir(os.path.join(account, bucket)):
                 print ('Syncing with bucket: "%s"' % bucket)
@@ -86,29 +108,62 @@ if __name__ == "__main__":
                         bucketdict[s3fileinfo["Key"]] = s3fileinfo
 
                 for key in localcontents:
-                    localfile = localcontents[key]
-                    if key in bucketdict:
-                        # file exists on s3 check lastmod
-                        if localfile["lastmod"] > bucketdict[key]["LastModified"]:
-                            print ("UPDATE %s (local:%s, remote:%s)" % (key,localfile["lastmod"],bucketdict[key]["LastModified"]))
-                            if not simulate:
-                                putfile(s3client,bucket,localfile,key)
+                    isIgnoreFile = False
+                    for ignore in ignoreList:
+                        if ignore in key:
+                            print ("Ignoring {}".format(key))
+                            isIgnoreFile = True
+                            break
+                    if not isIgnoreFile:        
+                        localfile = localcontents[key]
+                        if key in bucketdict:
+                            # file exists on s3 check lastmod
+                            if localfile["lastmod"] > bucketdict[key]["LastModified"]:
+                                print ("UPDATE %s (local:%s, remote:%s)" % (key,localfile["lastmod"],bucketdict[key]["LastModified"]))
+                                if not simulate:
+                                    putfile(s3client,bucket,localfile,key, noCacheFiles, changes)
+                            else:
+                                if checkmime:
+                                    filemd = s3client.head_object(Bucket=bucket, Key=key)
+                                    if filemd["ContentType"] != localfile['mime-type']:
+                                        print ("UPDATE %s (local mime:%s, remote mime:%s)" % (key,localfile['mime-type'],filemd["ContentType"]))
+                                        putfile(s3client,bucket,localfile,key, noCacheFiles, changes)
+                                #    else:
+                                #        print ('Checking %s mime type - MATCHES' % key)
+                            del bucketdict[key]
                         else:
-                            if checkmime:
-                                filemd = s3client.head_object(Bucket=bucket, Key=key)
-                                if filemd["ContentType"] != localfile['mime-type']:
-                                    print ("UPDATE %s (local mime:%s, remote mime:%s)" % (key,localfile['mime-type'],filemd["ContentType"]))
-                                    putfile(s3client,bucket,localfile,key)
-                            #    else:
-                            #        print ('Checking %s mime type - MATCHES' % key)
-                        del bucketdict[key]
-                    else:
-                        print ("ADD %s" % key)
-                        if not simulate:
-                            putfile(s3client,bucket,localfile,key)
+                            print ("ADD %s" % key)
+                            if not simulate:
+                                putfile(s3client,bucket,localfile,key, noCacheFiles, changes)
                 for key in bucketdict:
-                    print ("DELETE %s" % key)
-                    if not simulate:
-                        deletefile(s3client,bucket,key)
+                    for ignore in ignoreList:
+                        if ignore in key:
+                            print ("Ignoring {}".format(key))
+                            isIgnoreFile = True
+                            break
+                    if not isIgnoreFile:
+                        print ("DELETE %s" % key)
+                        if not simulate:
+                            deletefile(s3client, bucket, key)
+                            changes.append("/" + key)
+                        
+        if not simulate:
+            #print ('Invalidating the following:')
+            #print (changes)
+            if changes:
+                batch = {
+                    'Paths': { 
+                        'Quantity': len(changes),
+                        'Items': changes 
+                    }, 
+                    'CallerReference': 'S3-sync-{}'.format(str(datetime.datetime.now()).replace(' ','_')) 
+                }
+                response = cloudfront.create_invalidation(DistributionId=distributionId, InvalidationBatch=batch)
+                invalidationId = response['Invalidation']['Id']
+                if waitForCache:
+                    print ('Waiting for cloudfront invalidation')
+                    waitForCloudfront.waitForInvalidation(cloudfront, distributionId, invalidationId)
+                else:
+                    print ('Cloudfront invalidation id {}'.format(invalidationId))
     # second level is bucket
     # thrid level is content
